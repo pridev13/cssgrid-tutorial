2f8a55adc4f542db8e55eaaf43df6335334b2385
pridev13 <m@mvhr.nl>
2020-02-26 19:14:21 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+function compileScss() {
+
+	return gulp.src('*/*.scss')
+		.pipe(sass())
+    .pipe(prefix(['> 0.1%', 'ie 8', 'ie 7'], { cascade: true })) // pass the file through autoprefixer
+    .pipe(gulp.dest('.'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+
+}
+
+function watchTask() {
+	gulp.watch(
+		['*/*.scss'],
+			gulp.parallel(compileScss)
+	);
+}
+
+function browserSyncInit() {
+
+	browserSync.init({
+	   startPath: '/CSS/cssgrid',
+	   proxy: "localhost"
+	});
+
+}
+
+exports.default = gulp.series(compileScss, gulp.parallel(browserSyncInit, watchTask));
